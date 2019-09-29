a9710dbf3bcb0a989121d2a7dc515b53e177b78b
Bhupeshverma <bhupesh.v94@gmail.com>
2019-09-29 12:50:47 +0530

    Updates

---
+++ precache-manifest.d31a5595da33711e46005ad8f5dc998e.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = [
+  {
+    "revision": "d09e8367561db5258053",
+    "url": "/hook-news/static/js/runtime~main.d09e8367.js"
+  },
+  {
+    "revision": "0e841bdbc86426c0116d",
+    "url": "/hook-news/static/js/main.0e841bdb.chunk.js"
+  },
+  {
+    "revision": "599c9d90cf0741878ed3",
+    "url": "/hook-news/static/js/1.599c9d90.chunk.js"
+  },
+  {
+    "revision": "96b9bffa13dd1110162e2bfdfa71e71c",
+    "url": "/hook-news/index.html"
+  }
+];
